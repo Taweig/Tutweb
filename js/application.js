f8f8e31c33a27fcbd200a81c0bfebe21c9592edc
timodewinter <timodewinter@gmail.com>
2013-11-20 14:42:58 +0100

    Added royalSlider & styling updates

--- js/application.js
+++ js/application.js
@@ -48,5 +48,48 @@ String.prototype.repeat = function(num) {
         }
       });//.addSliderSegments($slider.slider("option").max);
     }
+    
+    $('#video-gallery').royalSlider({
+      arrowsNav: false,
+      fadeinLoadedSlide: true,
+      controlNavigationSpacing: 0,
+      controlNavigation: 'thumbnails',
+  
+/*
+      thumbs: {
+        autoCenter: false,
+        fitInViewport: true,
+        orientation: 'vertical',
+        spacing: 0,
+        paddingBottom: 0
+      },
+*/
+      keyboardNavEnabled: true,
+      imageScaleMode: 'fill',
+      imageAlignCenter:true,
+      slidesSpacing: 0,
+      loop: false,
+      loopRewind: true,
+      numImagesToPreload: 3,
+      video: {
+        autoHideArrows:true,
+        autoHideControlNav:false,
+        autoHideBlocks: true
+      },
+      thumbs: {
+        appendSpan: true,
+        firstMargin: true,
+        paddingBottom: 0,
+        spacing: 20
+      },
+      autoScaleSlider: true, 
+      autoScaleSliderWidth: 960,     
+      autoScaleSliderHeight: 580,
+  
+      /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+      imgWidth: 640,
+      imgHeight: 360
+  
+    });
 
 })(jQuery);
