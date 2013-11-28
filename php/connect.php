d794bc140e5ec91d874882b8a816f9cf7d3aef3a
timodewinter <timodewinter@gmail.com>
2013-11-28 15:38:34 +0100

    Use config.php for storing database credentials

--- php/connect.php
+++ php/connect.php
@@ -1,13 +1,8 @@
 <?php
-$db_user = 'root';
-$db_pass = '';
-$db_host = 'localhost';
-$db_name = 'NOM';
-
+require_once("config.php")
 $con= @mysql_connect("$db_host","$db_user","$db_pass")
 or die ("Kan geen verbinding maken met MySQL.");
 
 $db = @mysql_select_db("$db_name",$con)
 or die ("kan geen verbinding maken met $db_name database. Check de details in de database connection file en probeer het nog een keer");
-
 ?>
